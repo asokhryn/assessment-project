3065de51b4afacba109b4e5efbdf3281afcfa387
asokhryn <andrii.sokhryn@globallogic.com>
2024-09-06 15:50:52 +0300

    added snackBars when forms is successfully updated

--- src/app/settings/components/setting-contact-form/setting-contact-form.component.ts
+++ src/app/settings/components/setting-contact-form/setting-contact-form.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {MatButton} from "@angular/material/button";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {SnackBarComponent} from "../../../shared/components/snack-bar/snack-bar.component";
 
 @Component({
   selector: 'app-setting-contact-form',
@@ -18,8 +20,10 @@ import {MatButton} from "@angular/material/button";
   templateUrl: './setting-contact-form.component.html',
   styleUrl: './setting-contact-form.component.css'
 })
-export class SettingContactFormComponent {
+export class SettingContactFormComponent implements OnInit{
   form: FormGroup;
+  private _snackBar = inject(MatSnackBar);
+  durationInSeconds = 5;
 
   constructor(private fb: FormBuilder) {
 
@@ -36,5 +40,22 @@ export class SettingContactFormComponent {
 
   onSubmit() {
     localStorage.setItem('userData', JSON.stringify(this.form.value));
+    this.openSnackBar('Data saved successfully!')
+  }
+
+  openSnackBar(msg: string) {
+    this._snackBar.openFromComponent(SnackBarComponent, {
+      duration: this.durationInSeconds * 1000,
+      data: {
+        message: msg,
+      }
+    });
+  }
+
+  ngOnInit() {
+    const savedData = localStorage.getItem('userData');
+    if (savedData) {
+      this.form.setValue(JSON.parse(savedData));
+    }
   }
 }
--- src/app/settings/components/settings/settings.component.ts
+++ src/app/settings/components/settings/settings.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {SelectComponent} from "../../../shared/components/select/select.component";
 import {SettingContactFormComponent} from "../setting-contact-form/setting-contact-form.component";
+import {SnackBarComponent} from "../../../shared/components/snack-bar/snack-bar.component";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-settings',
@@ -26,7 +28,19 @@ export class SettingsComponent {
     { value: 'Japanese', viewValue: 'Japanese' }
   ];
 
+  private _snackBar = inject(MatSnackBar);
+  durationInSeconds = 5;
+
+  openSnackBar(msg: string) {
+    this._snackBar.openFromComponent(SnackBarComponent, {
+      duration: this.durationInSeconds * 1000,
+      data: {
+        message: msg,
+      }
+    });
+  }
+
   onLanguageSelected($event: string) {
-    console.log($event)
+    this.openSnackBar(`Your language "${$event}" is selected.`);
   }
 }
