b59742f1d387ae09629b3f7073c45f784f8e7e96
asokhryn <andrii.sokhryn@globallogic.com>
2024-09-06 17:38:04 +0300

    installed ng-apexcharts to project and added some diagrams to dashboard

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -1,12 +1,70 @@
 import { Component } from '@angular/core';
+import {MatCard} from "@angular/material/card";
+import {NgApexchartsModule} from "ng-apexcharts";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    NgApexchartsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  public pieChartOptions: any = {
+    chart: {
+      type: 'pie',
+      foreColor: '#0c0404'
+    },
+    labels: ['30+ years', '40+ years', '20+ years', '50+ years'],
+    series: [44, 55, 13, 43],
+    colors: ['#2c2c2c', '#5e5e5e', '#a9a9a9', '#d9d9d9'],
+    responsive: [{
+      breakpoint: 480,
+      options: {
+        chart: {
+          width: 200
+        },
+        legend: {
+          position: 'bottom'
+        }
+      }
+    }]
+  };
 
+  public lineChartOptions: any = {
+    chart: {
+      type: 'line',
+      height: 350,
+      foreColor: '#0c0c0c'
+    },
+    series: [{
+      name: 'Sales',
+      data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+    }],
+    xaxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+      labels: {
+        style: {
+          colors: ['#0c0c0c']
+        }
+      }
+    },
+    yaxis: {
+      labels: {
+        style: {
+          colors: ['#0c0c0c']
+        }
+      }
+    },
+    stroke: {
+      curve: 'smooth',
+      colors: ['#FFFFFF']
+    },
+    grid: {
+      borderColor: '#5e5e5e'
+    }
+  };
 }
